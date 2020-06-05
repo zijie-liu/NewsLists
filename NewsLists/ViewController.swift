ddb6068ae214509c75b99b3a4f33ba2f9603f190
Jie Liu <jie.01.dev@gmail.com>
2020-06-05 08:30:44 -0500

    Initial Commit

---
+++ NewsLists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsLists
+//
+//  Created by Peter on 6/5/20.
+//  Copyright © 2020 Jie Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
