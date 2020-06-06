deb54146b3cf22f386f6223cfa4a8a36640adf65
Jie Liu <jie.01.dev@gmail.com>
2020-06-06 02:35:01 -0500

    Added progress view in webview

--- NewsLists/View/WebViewController.swift
+++ NewsLists/View/WebViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 final class WebViewController: UIViewController, WKNavigationDelegate {
     
     @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet weak var progressView: UIProgressView!
     private var urlString: String?
     
     override func viewDidLoad() {
@@ -26,6 +27,21 @@ final class WebViewController: UIViewController, WKNavigationDelegate {
             let url = URL(string: urlString) {
             let urlRequest = URLRequest(url: url)
             webView.load(urlRequest)
+        }
+        webView.addObserver(self, forKeyPath: "estimatedProgress", options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        switch keyPath {
+        case "estimatedProgress":
+            if webView.estimatedProgress < 1.0 {
+                progressView.setProgress(Float(webView.estimatedProgress), animated: true)
+            } else {
+                progressView.setProgress(1.0, animated: true)
+                progressView.isHidden = true
+            }
+        default:
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
     }
     
